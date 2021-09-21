f3befc72aca73521ed8ebff72626e0767059c47c
Romain Storaï <romain.storai62@gmail.com>
2021-09-21 08:53:10 +0200

    Première Séance

--- Exercice Préparation Séance 2/src/fr/rstr/Etudiant.java
+++ Exercice Préparation Séance 2/src/fr/rstr/Etudiant.java
@@ -10,10 +10,22 @@ public class Etudiant {
         this.promo = promo;
     }
 
+    /* ====================
+     *      Méthodes
+     * ====================
+     */
+
+    /**
+     * Fait dire bonjour à l'étudiant actuel
+     */
     public void direBonjour() {
         System.out.printf("Bonjour je m'appelle %s %s, je suis de la promo %s%n", prenom, nom, promo);
     }
 
+    /**
+     * Fait dire bonjour à l'étudiant 2
+     * @param etudiant Etudiant 2
+     */
     public void direBonjourA(Etudiant etudiant) {
         if (etudiant.promo.equals(promo))
             System.out.printf("Salut %s, je m’appelle %s%n", etudiant.prenom, prenom);
--- Exercice Préparation Séance 2/src/fr/rstr/Main.java
+++ Exercice Préparation Séance 2/src/fr/rstr/Main.java
@@ -7,9 +7,9 @@ public class Main {
         Etudiant b = new Etudiant("Sardi", "Manon", "FIG172");
         Etudiant c = new Etudiant("Peira", "Dylan", "INFRES");
 
-        a.direBonjour();
-        b.direBonjourA(a);
-        c.direBonjourA(b);
+        a.direBonjour(); // Bonjour je m'appelle Romain Storaï, je suis de la promo FIG172
+        b.direBonjourA(a); // Salut Romain, je m’appelle Manon
+        c.direBonjourA(b); // Bonjour je m'appelle Dylan Peira, je suis de la promo INFRES
     }
 
 }
